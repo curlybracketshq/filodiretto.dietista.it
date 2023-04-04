0e8a506c1dbf36be165bb3f890b4103246e711a3
Giovanni Cappellotto <potomak84@gmail.com>
2023-04-03 21:46:56 -0400

    Update calendar representation

--- calendar/index.js
+++ calendar/index.js
@@ -26,54 +26,99 @@ function displayCalendar(token) {
       content.style.display = "block";
 
       const appointmentsResponse = JSON.parse(success.content);
-      const appointmentsByMonth = appointmentsResponse.Items.reduce(function (/** @type {Object.<string, Appointment[]>} */ collection, /** @type {Appointment} */ element) {
+      const [appointmentsByDay, months] = appointmentsResponse.Items.reduce(function (/** @type {[Object.<string, Appointment[]>, Object.<string, Boolean>]} */[appointmentsByDay, months], /** @type {Appointment} */ element) {
         const [date, _time] = element.datetime.S.split('T');
         const [year, month, _day] = date.split('-');
         const yearMonth = `${year}-${month}`;
-        if (yearMonth in collection) {
-          collection[yearMonth].push(element);
+        months[yearMonth] = true;
+        if (date in appointmentsByDay) {
+          appointmentsByDay[date].push(element);
         } else {
-          collection[yearMonth] = [element];
+          appointmentsByDay[date] = [element];
         }
-        return collection;
-      }, {});
-      const sortedMonths = Object.keys(appointmentsByMonth).sort();
+        return [appointmentsByDay, months];
+      }, [{}, {}]);
+      const sortedMonths = Object.keys(months).sort();
       calendar.innerHTML = sortedMonths.map((/** @type {string} */ yearMonth) => {
         const [year, month] = yearMonth.split('-');
-        const monthlyAppointments = appointmentsByMonth[yearMonth].sort(function (/** @type {{ datetime: { S: number; }; }} */ a, /** @type {{ datetime: { S: number; }; }} */ b) {
-          if (a.datetime.S < b.datetime.S) {
-            return -1;
+        const [yearNumber, monthNumber] = [parseInt(year, 10), parseInt(month, 10) - 1];
+        const beginningOfMonth = new Date(yearNumber, monthNumber, 1);
+        const beginningOfPrevMonth = new Date(yearNumber, monthNumber - 1, 1);
+        const beginningOfNextMonth = new Date(yearNumber, monthNumber + 1, 1);
+        const daysInMonth = Math.round((beginningOfNextMonth.getTime() - beginningOfMonth.getTime()) / 1000 / 60 / 60 / 24);
+        const endOfMonth = new Date(yearNumber, monthNumber, daysInMonth);
+        const daysInPrevMonth = Math.round((beginningOfMonth.getTime() - beginningOfPrevMonth.getTime()) / 1000 / 60 / 60 / 24);
+        let calendarMonthWeeks = [[]];
+        let week = 0;
+        let prevMonthDay = daysInPrevMonth - beginningOfMonth.getUTCDay() + 1;
+        for (let day = 0; day < beginningOfMonth.getUTCDay(); day++) {
+          calendarMonthWeeks[week].push(`
+          <td>
+            <div class="day prev_month">
+              <div class="number">${prevMonthDay}</div>
+              <div class="appointments"></div>
+            </div>
+          </td>`);
+          prevMonthDay++;
+        }
+        for (let day = 1; day <= daysInMonth; day++) {
+          const date = new Date(yearNumber, monthNumber, day);
+          const dayOfWeek = date.getUTCDay();
+          const [isoDate, _time] = date.toISOString().split('T');
+          const dailyAppointments = appointmentsByDay[isoDate] == null ? [] : appointmentsByDay[isoDate];
+          const dailyAppointmentsItems = dailyAppointments.sort(function (/** @type {{ datetime: { S: number; }; }} */ a, /** @type {{ datetime: { S: number; }; }} */ b) {
+            if (a.datetime.S < b.datetime.S) {
+              return -1;
+            }
+            if (a.datetime.S > b.datetime.S) {
+              return 1;
+            }
+            return 0;
+          }).map(function (/** @type {Appointment} */ element) {
+            const [_date, time] = element.datetime.S.split('T');
+            return `<li><a href="/appointments/#${element.from.S}|${element.datetime.S}">${time} (${element.from.S})</a></li>`;
+          }).join('');
+          calendarMonthWeeks[week].push(`
+          <td>
+            <div class="day ${dayOfWeek == 0 || dayOfWeek == 6 ? 'weekend' : ''}">
+              <div class="number">${day}</div>
+              <div class="appointments">
+                <ul>${dailyAppointmentsItems}</ul>
+              </div>
+            </div>
+          </td>`);
+          if (dayOfWeek == 6) {
+            calendarMonthWeeks.push([]);
+            week++;
           }
-          if (a.datetime.S > b.datetime.S) {
-            return 1;
-          }
-          return 0;
-        }).map(function (/** @type {Appointment} */ element) {
-          const [date, time] = element.datetime.S.split('T');
-          const dateObj = new Date(date);
-          const weekday = dateObj.getUTCDay();
-          const day = dateObj.getUTCDate();
-          return `
-          <tr>
-            <td>${weekdayName(weekday)} ${day}</td>
-            <td>${time}</td>
-            <td>${element.from.S}</td>
-            <td><a href="/appointments/#${element.from.S}|${element.datetime.S}">Dettagli</a></td>
-          </tr>`;
-        }).join('');
+        }
+        let nextMonthDay = 1;
+        for (let day = endOfMonth.getUTCDay(); day < 6; day++) {
+          calendarMonthWeeks[week].push(`
+          <td>
+            <div class="day next_month">
+              <div class="number">${nextMonthDay}</div>
+              <div class="appointments"></div>
+            </div>
+          </td>`);
+          nextMonthDay++;
+        }
 
         return `
         <h2>${monthName(parseInt(month, 10))} ${year}</h2>
         <table>
         <thead>
         <tr>
-          <th>Data</th>
-          <th>Ora</th>
-          <th>Numero</th>
-          <th>Operazioni</th>
+          <th class="weekend">D</th>
+          <th>L</th>
+          <th>M</th>
+          <th>M</th>
+          <th>G</th>
+          <th>V</th>
+          <th class="weekend">S</th>
         </tr>
         </thead>
-        <tbody>${monthlyAppointments}</tbody>
+        <tbody>${calendarMonthWeeks.map(week => `<tr>${week.join('')}</tr>`).join('')}</tbody>
         </table>`;
       }).join('');
     });
