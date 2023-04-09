f99bcd0a48ac08fe96e695c70498bd1aa6077353
Giovanni Cappellotto <potomak84@gmail.com>
2023-04-09 14:09:00 -0400

    Fix CORS decorator

--- _server/common/cors.py
+++ _server/common/cors.py
@@ -18,7 +18,7 @@ def access_control(methods):
             headers = response.get('headers', {})
             headers['Access-Control-Allow-Origin'] = '*'
             headers['Access-Control-Allow-Methods'] = ','.join(methods)
-            headers['Access-Control-Allow-Headers'] = 'Content-Type',
+            headers['Access-Control-Allow-Headers'] = 'Content-Type'
             response['headers'] = headers
             return response
         return new_f
