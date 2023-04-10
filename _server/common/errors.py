2fd43861256956f275f07b4ab05076d0e3a1c882
Giovanni Cappellotto <potomak84@gmail.com>
2023-04-09 21:51:39 -0400

    Add request info to error notification

--- _server/common/errors.py
+++ _server/common/errors.py
@@ -4,14 +4,16 @@ import traceback
 import os
 
 
-def _send_error_message():
+def _send_error_message(event):
     WEBHOOK_ID = os.environ.get('DISCORD_WEBHOOK_ID')
     WEBHOOK_TOKEN = os.environ.get('DISCORD_WEBHOOK_TOKEN')
     if WEBHOOK_ID is None or WEBHOOK_TOKEN is None:
         print("WEBHOOK_ID/WEBHOOK_TOKEN NOT SET")
         return
 
-    params = json.dumps({"content": traceback.format_exc()})
+    params = json.dumps({
+        "content": event['httpMethod'] + " " + event['path'] + "\n\n" + traceback.format_exc(),
+    })
     headers = {"Content-type": "application/json"}
     conn = http.client.HTTPSConnection("discord.com")
     conn.request("POST", "/api/webhooks/" + WEBHOOK_ID + "/" + WEBHOOK_TOKEN, params, headers)
@@ -28,7 +30,7 @@ def notify_discord(f):
         try:
             return f(event, context)
         except Exception:
-            _send_error_message()
+            _send_error_message(event)
 
         return {"statusCode": 500, "body": "Server error"}
     return new_f
