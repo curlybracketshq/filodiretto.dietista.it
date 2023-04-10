95feed1556cfd91a323c6fc6e15e0ab90ff7c0f3
Giovanni Cappellotto <potomak84@gmail.com>
2023-04-09 21:30:29 -0400

    Update discord webhook env vars names

--- _server/common/errors.py
+++ _server/common/errors.py
@@ -5,8 +5,8 @@ import os
 
 
 def _send_error_message(e):
-    WEBHOOK_ID = os.environ.get('WEBHOOK_ID')
-    WEBHOOK_TOKEN = os.environ.get('WEBHOOK_TOKEN')
+    WEBHOOK_ID = os.environ.get('DISCORD_WEBHOOK_ID')
+    WEBHOOK_TOKEN = os.environ.get('DISCORD_WEBHOOK_TOKEN')
     if WEBHOOK_ID is None or WEBHOOK_TOKEN is None:
         print("WEBHOOK_ID/WEBHOOK_TOKEN NOT SET")
         return
