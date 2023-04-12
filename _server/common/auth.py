4ab7cb1f47de5e36385ed7895b2f38a9ce47c795
Giovanni Cappellotto <potomak84@gmail.com>
2023-04-12 05:27:58 -0400

    Update auth.py

--- _server/common/auth.py
+++ _server/common/auth.py
@@ -18,7 +18,7 @@ def _is_token_valid(token):
 def require_auth(f):
     def new_f(event, context):
         token = None
-        if 'token' in event.get('queryStringParameters', {}):
+        if event['queryStringParameters'] is not None and 'token' in event['queryStringParameters']:
             token = json.loads(event['queryStringParameters']['token'])
 
         if token is None:
