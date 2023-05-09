43e70f110ad8e679ff04fe5d8047e4539836afc3
Giovanni Cappellotto <potomak84@gmail.com>
2023-05-08 21:22:02 -0400

    Don't store emtpy strings as numbers in dynamodb

--- _server/functions/filoDirettoConversation/lambda_function.py
+++ _server/functions/filoDirettoConversation/lambda_function.py
@@ -78,11 +78,12 @@ def lambda_handler(event, context):
                 '#H': 'height',
                 '#N': 'notes',
             }
+            height = body['conversation'].get('height', '')
             kwargs['ExpressionAttributeValues'] = {
                 ':f': {'S': body['conversation']['from']},
                 ':fn': {'S': body['conversation'].get('first_name', '')},
                 ':ln': {'S': body['conversation'].get('last_name', '')},
-                ':h': {'N': body['conversation'].get('height', '')},
+                ':h': {'N': height if len(height) > 0 else None},
                 ':n': {'S': body['conversation'].get('notes', '')},
             }
             kwargs['UpdateExpression'] = 'SET #FN = :fn, #LN = :ln, #H = :h, #N = :n'
@@ -94,6 +95,7 @@ def lambda_handler(event, context):
         body = json.loads(event['body'])
 
         dynamodb = boto3.client('dynamodb')
+        height = body['conversation'].get('height', '')
         # Add a new sender conditionally so we don't overwrite existing items
         result = dynamodb.put_item(
             TableName=SENDERS_TABLE,
@@ -101,7 +103,7 @@ def lambda_handler(event, context):
                 'from': {'S': body['conversation']['from']},
                 'firstName': {'S': body['conversation'].get('first_name', '')},
                 'lastName': {'S': body['conversation'].get('last_name', '')},
-                'height': {'N': body['conversation'].get('height', '')},
+                'height': {'N': height if len(height) > 0 else None},
                 'notes': {'S': body['conversation'].get('notes', '')},
             },
             ExpressionAttributeNames={
